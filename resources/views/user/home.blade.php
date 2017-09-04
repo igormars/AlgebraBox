749f2fe9aab3b5e3fed4cd32290d4d2f1f98fede
online3 <online3@ucione.local>
2017-09-04 21:07:50 +0200

    Predavanje 4-9-2017

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -9,6 +9,19 @@
   </ol>
 </div>
 <div class="row">
-
+	<div class="col-md-3">
+		<div class="list-group">
+			<a href="#" class="list-group-item active">
+				Create New Directory
+			</a>
+			<a href="#" class="list-group-item list-group-item-info">Dapibus ac facilisis in</a>
+			<a href="#" class="list-group-item list-group-item-info">Morbi leo risus</a>
+			<a href="#" class="list-group-item list-group-item-info">Porta ac consectetur ac</a>
+			<a href="#" class="list-group-item list-group-item-info">Vestibulum at eros</a>
+		</div>
+	</div>
+	<div class="col-md-9">
+	{{ print_r($directories->name) }}
+	</div>
 </div>
 @stop
